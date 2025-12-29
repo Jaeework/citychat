9d07ed4bbb55c785d5110fba762a60eb42162fce
Jaeework <jaeework@gmail.com>
2025-12-30 02:52:45 +0900

    refactor: #6 City 엔티티 및 dto 수정

--- backend/domain/entities/City.ts
+++ backend/domain/entities/City.ts
@@ -2,7 +2,8 @@ export class City {
   constructor(
     public readonly id: number,
     public readonly name: string,
-    public readonly description: string
+    public readonly description: string,
+    public readonly image: string, // 가공된 url
   ) {}
 }
 
