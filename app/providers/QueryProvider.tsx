a2582d9e3ef20ecd7fdb51222a7df71b1b54e40b
Jaeework <jaeework@gmail.com>
2026-02-04 14:45:32 +0900

    fix: #10 QueryClient 인스턴스 생성 방식 개선
    - Tanstack Query ESLint 규칙 따라 `useState` 사용
    - useState 사용으로 리렌더링 시 QueryClient 재생성 방지

--- app/providers/QueryProvider.tsx
+++ app/providers/QueryProvider.tsx
@@ -1,18 +1,22 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false, // 창이 포커스될 때 자동으로 다시 가져오지 않음
-        retry: 1, // 실패 시 재시도 횟수
-      },
-      mutations: {
-        retry: 1, // 실패 시 재시도 횟수
-      },
-    }});
+  const [queryClient] = useState(
+    () => new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false, // 창이 포커스될 때 자동으로 다시 가져오지 않음
+          retry: 1, // 실패 시 재시도 횟수
+        },
+        mutations: {
+          retry: 1, // 실패 시 재시도 횟수
+        },
+      }
+    })
+  );
 
   return (
     <QueryClientProvider client={queryClient}>
